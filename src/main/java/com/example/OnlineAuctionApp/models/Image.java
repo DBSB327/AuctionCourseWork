ad9e48db1cfa02ee389ed588d1ecaa10599b7873
Askat <kaskat6572@gmail.com>
2023-05-10 22:55:22 +0600

    image

---
+++ src/main/java/com/example/OnlineAuctionApp/models/Image.java
@@ -0,0 +1,26 @@
+package com.example.OnlineAuctionApp.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "type")
+    private String type;
+    @Lob
+    @Column(name = "image_data", length = 1000)
+    private byte[] imageData;
+}
